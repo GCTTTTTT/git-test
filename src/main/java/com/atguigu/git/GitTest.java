158dfb97510d14e8d4bb53c239d874f26d2c121e
GCTTTTTT <1587558540@qq.com>
2022-05-14 20:16:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @projectName: git-test
+ * @package: com.atguigu.git
+ * @className: GitTest
+ * @author: GCT
+ * @description: TODO
+ * @date: 2022/5/14 20:05
+ * @version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hhehrn");
+    }
+}
